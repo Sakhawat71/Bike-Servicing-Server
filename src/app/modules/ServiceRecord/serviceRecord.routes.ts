57a65d21c02ff711a3ac56e9bd627e96f5ac8aed
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-04-19 02:47:46 +0600

    for naming porblem  "git config core.ignorecase false"

--- src/app/modules/ServiceRecord/serviceRecord.routes.ts
+++ src/app/modules/ServiceRecord/serviceRecord.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { ServiceRecordController } from "./ServiceRecord.controller";
+import { ServiceRecordController } from "./serviceRecords.controller";
 
 
 const router = Router();
